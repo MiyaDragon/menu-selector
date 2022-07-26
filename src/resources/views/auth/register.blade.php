433d843a17792d643c0b157de8716b295eb5a094
ryutaromiyagawa <miyagaworld_0912@icloud.com>
2022-07-26 16:42:05 +0900

    [add] #13 ユーザー登録画面作成

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,76 +1,44 @@
-@extends('layouts.app')
+@extends('app')
+
+@section('title', 'ユーザー登録')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="container" style="max-width: 540px">
+    <h1 class="text-center">献立セレクター</h1>
+    <div class="card mt-3">
+        <div class="card-body">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            <h2 class="card-title text-center mt-2">ユーザー登録</h2>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+            @include('error_card_list')
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group">
+                    <label for="name">ユーザー名</label>
+                    <input type="text" class="form-control" id="name" name="name" required value="{{ old('name') }}">
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input type="text" class="form-control" id="email" name="email" required value="{{ old('email') }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">パスワード（確認）</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+
+                </div>
+
+                <button type="submit" class="btn btn-block btn-primary mt-4">ユーザー登録</button>
+
+            </form>
         </div>
     </div>
 </div>
--- src/resources/views/nav.blade.php
+++ src/resources/views/nav.blade.php
@@ -4,13 +4,33 @@
 
     <ul class="navbar-nav ml-auto">
 
+        @guest
         <li class="nav-item">
-            <a class="nav-link" href="">ユーザー登録</a>
+            <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
         </li>
+        @endguest
 
+        @guest
         <li class="nav-item">
-            <a class="nav-link" href="">ログイン</a>
+            <a class="nav-link" href="{{ route('login') }}">ログイン</a>
         </li>
+        @endguest
+
+        @auth
+        <li class="nav-item">
+            <a class="nav-link" href="">マイページ</a>
+        </li>
+        @endauth
+
+        @auth
+        <li class="nav-item">
+            <form method="POST" action="{{ route('logiout') }}">
+                @csrf
+                <button class="nav-link" type="submit">ログアウト</button>
+            </form>
+        </li>
+        @endauth
+
     </ul>
 
 </nav>
