09ed1da381dfd44ff8eaf54d4707d34a9d2e9bde
ryutaromiyagawa <miyagaworld_0912@icloud.com>
2022-08-03 23:46:06 +0900

    [update] #43 Top画面のUI調整

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -5,10 +5,10 @@
 @section('content')
 @include('nav')
 <div class="container text-center">
-    <div class="card mt-3 mx-auto" style="width: 500px;">
-        <img src="{{ asset('storage/noImage.jpeg') }}" class="card-img-top" alt="...">
+    <div class="card mt-3 mx-auto" style="max-width: 500px">
+        <img src="{{ asset('storage/noImage.jpeg') }}" class="card-img-top" alt="食品の画像">
         <div class="card-body">
-            <h3 class="card-title">{{ $menu->name ?? '今日の献立は...' }}</h3>
+            <h3 class="card-title pb-2 skyblue">{{ $menu->name ?? '今日の献立は...' }}</h3>
             <form method="POST" action="{{ route('show') }}">
                 @csrf
                 <div class="card-text row">
@@ -16,15 +16,15 @@
                         <label for="genre" class="col-form-label">ジャンル</label>
                     </div>
                     <div class="col-10">
-                        <select name="genre_id" class="form-control" id="genre">
-                            <option style="display: none;" value="all">▽選択してください</option>
+                        <select name="genre_id" class="form-select" id="genre">
+                            <option value="all">全てのジャンル</option>
                             @foreach($genres as $genre)
                             <option value="{{ $genre->id }}">{{ $genre->name }}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary mt-3">決定</button>
+                <button type="submit" class="btn btn-warning mt-4 px-5">決定</button>
             </form>
         </div>
     </div>
