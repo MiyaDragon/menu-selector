66b2f350ed719d32111db70d0bcff84e09e98050
ryutaromiyagawa <miyagaworld_0912@icloud.com>
2022-08-05 16:59:56 +0900

    [edit] #58 献立登録画面のUI修正

--- src/resources/views/menus/create.blade.php
+++ src/resources/views/menus/create.blade.php
@@ -4,34 +4,38 @@
 
 @section('content')
 @include('nav')
-<div class="container" style="max-width: 540px">
-    <h1 class="text-center mt-2">献立セレクター</h1>
-    <div class="card mt-3">
-        <div class="card-body">
-
-            <h2 class="card-title text-center mt-2">献立登録</h2>
-
-            @include('error_card_list')
+<div class="container" style="max-width: 450px">
+    <h1 class="h3 mt-4 text-center fw-bolder">献立登録</h1>
+    <div class="card mt-3 mx-auto">
+        <div class="card-body mt-3 mx-4">
 
             <form method="POST" action="{{ route('menus.store') }}" enctype="multipart/form-data">
                 @csrf
 
                 <div class="form-group">
                     <label for="menu_name">献立名</label>
-                    <input type="text" class="form-control" id="menu_name" name="menu_name" required value="{{ old('menu_name') }}">
+                    <sapn class="text-danger small">必須</sapn>
+                    <input type="text" class="form-control @error('menu_name') is-invalid @enderror" id="menu_name" name="menu_name" required value="{{ old('menu_name') }}">
+                    @include('error_input_under', ['name' => 'menu_name'])
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mt-3">
                     <label for="genre_name">ジャンル名</label>
-                    <input type="text" class="form-control" id="genre_name" name="genre_name" required value="{{ old('genre_name') }}">
+                    <sapn class="text-danger small">必須</sapn>
+                    <input type="text" class="form-control @error('genre_name') is-invalid @enderror" id="genre_name" name="genre_name" required value="{{ old('genre_name') }}">
+                    @include('error_input_under', ['name' => 'genre_name'])
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mt-3">
                     <label for="menu_image">画像</label>
-                    <input type="file" class="form-control" id="menu_image" name="menu_image">
+                    <p class="text-muted small my-1">※画像は .jpg .png に対応しています。</p>
+                    <input type="file" class="form-control @error('menu_image') is-invalid @enderror" id="menu_image" name="menu_image">
+                    @include('error_input_under', ['name' => 'menu_image'])
                 </div>
 
-                <button type="submit" class="btn btn-block btn-primary mt-4">献立登録</button>
+                <div class="d-grid gap-2 my-4">
+                    <button type="submit" class="btn btn-warning text-white btn-lg">登録する</button>
+                </div>
 
             </form>
         </div>
