f9fb2b12201f934df1393a5437eec4cfd05da2c7
ryutaromiyagawa <miyagaworld_0912@icloud.com>
2022-09-07 00:00:04 +0900

    [update] #198 navbarレスポンシブ対応

--- src/resources/views/nav.blade.php
+++ src/resources/views/nav.blade.php
@@ -1,24 +1,33 @@
-<nav class="py-2 bg-orange-gradation border-bottom sticky-top">
+<nav class="navbar navbar-expand-md  bg-orange-gradation sticky-top">
     <div class="container d-flex flex-wrap">
-        <ul class="nav me-auto">
-            <li class="nav-item">
-                <a class="nav-link link-white px-2 fs-5 text-white fw-bolder d-flex" href="/">
-                    <img src="{{ asset('images/logo.png') }}" class="me-2" alt="">
-                    <div class="d-flex align-items-center justify-content-center">献立セレクター</div>
-                </a>
-            </li>
-        </ul>
-        <ul class="nav d-flex align-items-center justify-content-center">
-            @guest
-            <li class="nav-item">
-                <a class="btn btn-success nav-link text-white px-2 me-2" href="{{ route('register') }}">ユーザー登録</a>
-            </li>
-            <li class="nav-item">
-                <a class="btn btn-outline-orange bg-white nav-link link-orange px-2" href="{{ route('login') }}">ログイン</a>
-            </li>
-            @endguest
-
-            @auth
+        <a class="navbar-brand link-white px-2 fs-5 text-white fw-bolder d-flex" href="{{ route('home') }}">
+            <img src="{{ asset('images/logo.png') }}" class="me-2" alt="menu">
+            <div class="d-flex align-items-center justify-content-center">献立セレクター</div>
+        </a>
+        @guest
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse my-1" id="navbarScroll">
+            <ul class="navbar-nav ms-auto my-md-0 navbar-nav-scroll">
+                <div class="d-none d-md-block">
+                    <li class="nav-item">
+                        <a class="btn btn-success nav-link text-white px-2 me-2" href="{{ route('register') }}">ユーザー登録</a>
+                    </li>
+                </div>
+                <div class="d-block d-md-none">
+                    <li class="nav-item">
+                        <a class="btn btn-success nav-link text-white px-2 mb-2" href="{{ route('register') }}">ユーザー登録</a>
+                    </li>
+                </div>
+                <li class="nav-item">
+                    <a class="btn btn-outline-orange bg-white nav-link link-orange px-2" href="{{ route('login') }}">ログイン</a>
+                </li>
+            </ul>
+        </div>
+        @endguest
+        @auth
+        <ul class="nav ms-auto">
             <li class="nav-item dropdown">
                 <a class="nav-link" type="button" aria-expanded="false" data-bs-toggle="dropdown">
                     <i class="fas fa-utensils fa-2x text-white"></i>
